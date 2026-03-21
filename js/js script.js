e3431a44e934ddb5d664cafd655e53fcb18fb819
natkanatkanatui1983 <ninimamporia19893@gmail.com>
2026-03-21 17:23:33 +0400

    ADD

---
+++ js/js script.js
@@ -0,0 +1,3 @@
+let navBar = document.getElementById('navigation');
+let burgerBar = document.getElementById('burger-bar');
+burgerBar.addEventListener('click', function () { navBar.classList.toggle('active'); });
