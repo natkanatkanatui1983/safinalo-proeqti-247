76444972130a69e038ea5af602c587351490ea27
natkanatkanatui1983 <ninimamporia19893@gmail.com>
2026-03-22 20:13:15 +0400

    add

--- js/js script.js
+++ js/js script.js
@@ -1,3 +1,14 @@
 let navBar = document.getElementById('navigation');
 let burgerBar = document.getElementById('burger-bar');
 burgerBar.addEventListener('click', function () { navBar.classList.toggle('active'); });
+
+
+
+
+
+let btnElement = document.getElementById('click-btn');
+btnElement.addEventListener('click', function () {
+    alert("you have successfully registered");
+
+    
+});
